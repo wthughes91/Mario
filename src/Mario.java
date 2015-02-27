fb2e3e00a55b96ae2e97327bb662257a8ca61aca
wthughes91 <trey.hughes91@gmail.com>
2015-02-27 16:27:48 -0500

    Now allows option of printing to a text file

--- src/Mario.java
+++ src/Mario.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Mario {
@@ -10,14 +13,43 @@ public class Mario {
             height = scan.nextInt();
         } while(height < 0 || height > 23);
 
-        for (int i = 1; i <= height; i++) {
-            for (int j = 1; j <= height - i; j++) {
-                System.out.print(" ");
+        Scanner check = new Scanner(System.in);
+        System.out.print("Do you want the pyramid printed in a text file? ");
+        String answer = check.nextLine();
+
+        if (answer.equals("yes") || answer.equals("y")) {
+            try {
+                File pyramid = new File("Mario.txt");
+                PrintWriter out = new PrintWriter(pyramid);
+                for (int i = 1; i <= height; i++) {
+                    for (int j = 1; j <= height - i; j++) {
+                        out.print(" ");
+                    }
+                    for (int k = 1; k <= i + 1; k++) {
+                        out.print("#");
+                    }
+                    out.print("\n");
+                }
+                out.close();
+            } catch (IOException exception) {
+                System.out.print("Unable to print");
             }
-            for (int k = 1; k <= i + 1; k++) {
-                System.out.print("#");
+        }
+
+        else if (answer.equals("no") || answer.equals("n")) {
+            for (int i = 1; i <= height; i++) {
+                for (int j = 1; j <= height - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int k = 1; k <= i + 1; k++) {
+                    System.out.print("#");
+                }
+                System.out.print("\n");
             }
-            System.out.print("\n");
+        }
+
+        else {
+            System.out.print("Unable to print");
         }
     }
 }
